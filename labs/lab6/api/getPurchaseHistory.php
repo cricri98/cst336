3f5506a10b9c070e9b1062dfaf851553cc78a3d5
Chrisjloks 23 <chrisjimenez.fba@gmail.com>
2019-03-26 17:21:46 +0000

    skelton

---
+++ labs/lab6/api/getPurchaseHistory.php
@@ -0,0 +1,20 @@
+<?php
+    include '../../../inc/dbConnection.php';
+    $conn = getDatabaseConnection("ottermart");
+    
+    $productId = $_GET['productId'];
+    $sql = "SELECT * FROM om_product 
+            NATURAL JOIN om_purchase
+            WHERE productId = :pId";
+            
+    $np = array();
+    $np[':pId'] = $productId;
+    
+    $stmt = $conn->prepare($sql);  //$connection MUST be previously initialized
+$stmt->execute($np);
+$records = $stmt->fetchAll(PDO::FETCH_ASSOC); //use fetch for one record, fetchAll for multiple
+
+//print_r($records); //debugging    
+
+echo json_encode($records);
+?>
