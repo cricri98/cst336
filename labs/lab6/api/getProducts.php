220a693b6c82ca8a870a67036b3759d000460b60
Chrisjloks 23 <chrisjimenez.fba@gmail.com>
2019-04-17 21:14:17 +0000

    changes to lab 7

--- labs/lab6/api/getProducts.php
+++ labs/lab6/api/getProducts.php
@@ -2,7 +2,7 @@
 
 //header('Access-Control-Allow-Origin: *');
 
-$host = "localhost";
+/*$host = "localhost";
 $dbname = "ottermart";
 $username = "root";
 $password = "";
@@ -11,10 +11,13 @@ $password = "";
 $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
 // Setting Errorhandling to Exception
-$dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+$dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); */
 
+include '../../inc/dbConnection.php';
+$conn = getDatabaseConnection("ottermart");
+    
 $sql = "SELECT * FROM om_product ORDER BY productPrice";
-$stmt = $dbConn -> prepare($sql);  //$connection MUST be previously initialized
+$stmt = $conn -> prepare($sql);  //$connection MUST be previously initialized
 $stmt->execute();
 $records = $stmt->fetchAll(PDO::FETCH_ASSOC); //use fetch for one record, fetchAll for multiple
 
